609e4fd57009dfca7e16c0d26e3d58b3ca019eec
Dan Feeney <daniel.feeney@colorado.edu>
2020-04-15 12:20:58 -0600

    Adding in chp 3 notes

--- RMCourse.R
+++ RMCourse.R
@@ -50,6 +50,21 @@ precis(globe.qa)
 # errors with Hessians (square matrix w/ 2nd derivatives). In this case, it is 2nd derivative of log of posterior
 # probability w.r.t parameters. A parabola does not have derivates beyond the 2nd, so this is sufficient to 
 # characterize the posterior. 
+## MCMC algorithm Markov Chain Monte Carlo
+n_samples <- 1000 
+p <- rep( NA , n_samples )
+p[1] <- 0.5
+W <- 6
+L <- 3
+for ( i in 2:n_samples ) {
+  p_new <- rnorm( 1 , p[i-1] , 0.1 )
+  if ( p_new < 0 ) p_new <- abs( p_new )
+  if ( p_new > 1 ) p_new <- 2 - p_new
+  q0 <- dbinom( W , W+L , p[i-1] )
+  q1 <- dbinom( W , W+L , p_new )
+}
+dens( p , xlim=c(0,1) )
+curve( dbeta( x , W+1 , L+1 ) , lty=2 , add=TRUE )
 # HW 1
 rm(list=ls())
 set.seed(5280)
